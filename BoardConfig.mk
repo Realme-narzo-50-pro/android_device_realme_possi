71dcf346a08e85e965074df6958b2eb52017d19e
Your Name <you@example.com>
2024-04-07 22:52:29 -0400

    added_vintf_fcm

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,6 +158,8 @@ BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 # Inherit the proprietary files
 include vendor/realme/RMX3395/BoardConfigVendor.mk
